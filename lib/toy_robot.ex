df6c12e69866cffd82d416bbfaa131b94b0be2b1
Fuad <kamilovfuad@gmail.com>
2022-04-24 18:51:18 +0400

    Add move function that makes robot move forward in the direction its facing

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -1,5 +1,20 @@
 defmodule ToyRobot.Robot do
   @doc """
+    Moves the robot forward one space in the direction it is facing
+
+    ## Examples
+      iex> alias ToyRobot.Robot
+      ToyRobot.Robot
+      iex> robot = %{north: 0, facing: :north}
+      %{north: 0, facing: :north}
+      iex> robot |> Robot.move
+      %{north: 1}
+  """
+  def move(robot) do
+
+  end
+
+  @doc """
   Moves the robot east one space.
 
   ## Examples
