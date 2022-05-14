f245317d1384f8aaee5aa03953bd3f3051b29ce9
Fuad <kamilovfuad@gmail.com>
2022-05-14 22:44:07 +0400

    add struct

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -1,18 +1,18 @@
 defmodule ToyRobot.Robot do
+  alias ToyRobot.Robot
+  defstruct [north: 0, east: 0, facing: :north]
   @doc """
     Moves the robot forward one space in the direction it is facing
 
     ## Examples
       iex> alias ToyRobot.Robot
       ToyRobot.Robot
-      iex> robot = %{north: 0, facing: :north}
-      %{north: 0, facing: :north}
+      iex> robot = %Robot{north: 0, facing: :north}
+      %Robot{north: 0, facing: :north}
       iex> robot |> Robot.move
-      %{north: 1}
+      %Robot{north: 1}
   """
-  def move(robot) do
-
-  end
+  def move(%__MODULE__{} = robot), do: robot |> move_north()
 
   @doc """
   Moves the robot east one space.
@@ -21,15 +21,15 @@ defmodule ToyRobot.Robot do
 
   iex> alias ToyRobot.Robot
   ToyRobot.Robot
-  iex> robot = %{east: 0}
-  %{east: 0}
+  iex> robot = %Robot{east: 0}
+  %Robot{east: 0}
   iex> robot |> Robot.move_east
-  %{east: 1}
+  %Robot{east: 1}
   iex> robot |> Robot.move_east |> Robot.move_east |> Robot.move_east
-  %{east: 3}
+  %Robot{east: 3}
   """
   def move_east(robot) do
-    %{east: robot.east + 1}
+    %Robot{east: robot.east + 1}
   end
 
   @doc """
